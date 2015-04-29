25129e8a3d60705ce2018f5fd0688655d87f824c
night-stalker <aartyomenko@cs.gsu.edu>
2015-04-28 20:45:42 -0400

    15 types

--- ml/scikit_test.py
+++ ml/scikit_test.py
@@ -7,11 +7,8 @@ from collections import Counter
 import pickle
 import json
 
-def grid_search_cv(samples, targets, data, k, tuned_params):
+def grid_search_cv(samples, targets, data, k, tuned_params, svr, path):
     mycv = cross_validation.StratifiedKFold(targets, n_folds = k)
-    svr = svm.LinearSVC()
-    #svr = svm.SVC()
-    #svr = ensemble.RandomForestClassifier()
 
     print mycv
 
@@ -20,7 +17,7 @@ def grid_search_cv(samples, targets, data, k, tuned_params):
     print clf.best_params_
     print clf.best_estimator_.predict(data[0])
     print len(data[0])
-    pickle.dump(clf.best_estimator_, open('SVMlin.pkl', 'w+'))
+    pickle.dump(clf.best_estimator_, open(path, 'w+'))
     return clf.best_estimator_
 
 def get_common_ids(target_names):
@@ -72,28 +69,44 @@ def train(clf, samples, targets, data, k):
 
 if __name__=='__main__':
 
-    #iris = datasets.load_iris()
-    #
-    # tr = train(iris, 10)
-    #
-    # print "Accuracy: %0.2f (+/- %0.2f)" % (tr[1], tr[2])
     target_names = {0: 'bladder.json', 1:'blood.json', 2: 'brain.json',
-                    3: 'breast.json', 4:'cervix.json'#, 5: 'colorectal.json',
-                    # 6: 'headneck.json', 7: 'kidney.json', 8: 'liver.json',
-                    #9: 'lung.json', 10: 'ovary.json', 11: 'pancreas.json',
-                    # 12: 'prostate.json', 13: 'skin.json', 14: 'uterus.json'
+                    3: 'breast.json', 4:'cervix.json', 5: 'colorectal.json',
+                    6: 'headneck.json', 7: 'kidney.json', 8: 'liver.json',
+                    9: 'lung.json', 10: 'ovary.json', 11: 'pancreas.json',
+                    12: 'prostate.json', 13: 'skin.json', 14: 'uterus.json'
                 }
     samples, targets, data = parse_json(target_names)
     print 'Loading finished!'
 
     target_indices = np.array(targets)
 
+    # Linear SVM
     parameters = {'C':[10], 'class_weight':['auto'],
                   'multi_class':['ovr'], 'penalty':['l2'] }
-   # parameters =  {'C': [1, 10, 100], 'gamma': [0.001, 0.0001], 'kernel': ['rbf', 'poly']}
-    #parameters = {"n_estimators": [10, 20, 50], "n_jobs": [-1],"class_weight":['auto','subsample', None] }
-    print "Grid search..."
-    clf = grid_search_cv(samples, target_indices, data, 5, parameters)
+    #
+    svr = svm.LinearSVC()
+
+    print "Linear SVM..."
+    clf = grid_search_cv(samples, target_indices, data, 5, parameters, svr, "SVMlin15.pickle")
     print "Validation..."
     tr = train(clf, samples, target_indices, data, 5)
     print "Accuracy: %0.2f (+/- %0.2f)" % (tr[1], tr[2])
+
+    # RandomForest
+    parameters = {"n_estimators": [10, 20, 50], "n_jobs": [-1],"class_weight":['auto','subsample', None] }
+    svr = ensemble.RandomForestClassifier()
+    print "Random Forest..."
+    clf = grid_search_cv(samples, target_indices, data, 5, parameters, svr, "RandomForest15.pickle")
+    print "Validation..."
+    tr = train(clf, samples, target_indices, data, 5)
+    print "Accuracy: %0.2f (+/- %0.2f)" % (tr[1], tr[2])
+
+    # SVM
+    parameters =  {'C': [1, 10, 100], 'gamma': [0.001, 0.0001], 'kernel': ['rbf', 'poly']}
+    svr = svm.SVC()
+    print "SVM..."
+    clf = grid_search_cv(samples, target_indices, data, 5, parameters, svr, "SVM15.pickle")
+    print "Validation..."
+    tr = train(clf, samples, target_indices, data, 5)
+    print "Accuracy: %0.2f (+/- %0.2f)" % (tr[1], tr[2])
+
